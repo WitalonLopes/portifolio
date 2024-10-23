dc4b95554590215f0b327fb3b38615ad9408e194
WitalonLopes <witalonlopez@gmail.com>
2024-10-22 22:46:47 -0300

    estilizando nav-list

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+//MENU LATERAL ATIVO
+
 const hamburger = document.querySelector(".hamburger");
 
 const nav = document.querySelector(".menu")
@@ -5,8 +7,16 @@ const nav = document.querySelector(".menu")
 hamburger.addEventListener("click", expandir);
 
 function expandir() {
- nav.classList.toggle("active");
+  nav.classList.toggle("active");
 }
+
+//SCROLL NO HEADER TRANSPARENTE 
+
+document.addEventListener('scroll', () => {
+  let header = document.querySelector("header#header")
+  header.classList.toggle('rolagem', window.scrollY > 100)
+})
+
 
 //PARALLAX
 
@@ -15,13 +25,13 @@ let onda2 = document.getElementById('onda2')
 let onda3 = document.getElementById('onda3')
 let onda4 = document.getElementById('onda4')
 
-window.addEventListener('scroll', function(){
-    let rolagemPos = window.scrollY
+window.addEventListener('scroll', function () {
+  let rolagemPos = window.scrollY
 
-    onda1.style.backgroundPositionX = 600 + rolagemPos * 5 + 'px'
-    onda2.style.backgroundPositionX = 500 + rolagemPos * -5 + 'px'
-    onda3.style.backgroundPositionX = 400 + rolagemPos * 3 + 'px'
-    onda4.style.backgroundPositionX = 300 + rolagemPos * -3 + 'px'
+  onda1.style.backgroundPositionX = 600 + rolagemPos * 5 + 'px'
+  onda2.style.backgroundPositionX = 500 + rolagemPos * -5 + 'px'
+  onda3.style.backgroundPositionX = 400 + rolagemPos * 3 + 'px'
+  onda4.style.backgroundPositionX = 300 + rolagemPos * -3 + 'px'
 })
 
 // ANIMAÇÃO DA TECNOLIAS
@@ -50,15 +60,15 @@ document.addEventListener('DOMContentLoaded', checkElementsVisibility);
 //TOOLTIPS
 
 document.addEventListener('DOMContentLoaded', () => {
-    const icons = document.querySelectorAll('.tecnologias i');
+  const icons = document.querySelectorAll('.tecnologias i');
 
-    icons.forEach(icon => {
-        const tooltipText = icon.getAttribute('data-tooltip');
-        const tooltip = document.createElement('div');
-        tooltip.className = 'tooltip';
-        tooltip.innerText = tooltipText;
-        icon.parentElement.appendChild(tooltip);
-    });
+  icons.forEach(icon => {
+    const tooltipText = icon.getAttribute('data-tooltip');
+    const tooltip = document.createElement('div');
+    tooltip.className = 'tooltip';
+    tooltip.innerText = tooltipText;
+    icon.parentElement.appendChild(tooltip);
+  });
 });
 
 // src/js/social-scroll.js
@@ -67,12 +77,12 @@ document.addEventListener('DOMContentLoaded', () => {
   const socialSection = document.querySelector('.social');
 
   const handleScroll = () => {
-      const socialRect = socialSection.getBoundingClientRect();
-      if (socialRect.bottom < 0) {
-          fixedSocial.style.display = 'flex';
-      } else {
-          fixedSocial.style.display = 'none';
-      }
+    const socialRect = socialSection.getBoundingClientRect();
+    if (socialRect.bottom < 0) {
+      fixedSocial.style.display = 'flex';
+    } else {
+      fixedSocial.style.display = 'none';
+    }
   };
 
   window.addEventListener('scroll', handleScroll);
@@ -80,21 +90,21 @@ document.addEventListener('DOMContentLoaded', () => {
 
 //FOOTER SCROLL LATERAL SUMINDO
 document.addEventListener('DOMContentLoaded', () => {
-const fixedSocial = document.querySelector('.fixed-social');
-const socialSection = document.querySelector('.social');
-const footerSection = document.querySelector('.rodape');
+  const fixedSocial = document.querySelector('.fixed-social');
+  const socialSection = document.querySelector('.social');
+  const footerSection = document.querySelector('.rodape');
 
-const handleScroll = () => {
+  const handleScroll = () => {
     const socialRect = socialSection.getBoundingClientRect();
     const footerRect = footerSection.getBoundingClientRect();
 
     if (socialRect.bottom < 0 && footerRect.top > window.innerHeight) {
-        fixedSocial.style.display = 'flex';
+      fixedSocial.style.display = 'flex';
     } else {
-        fixedSocial.style.display = 'none';
+      fixedSocial.style.display = 'none';
     }
-};
+  };
 
-window.addEventListener('scroll', handleScroll);
+  window.addEventListener('scroll', handleScroll);
 });
 
